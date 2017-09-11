97080480622fb81ed0b23f1d01c860b5f3e0d8f0
stefanc <stefan@themeisle.com>
2017-09-11 13:54:29 +0300

    #15 Add notification in dashboard and customizer
    Notification that the theme is not maintained anymore added in dahsboard and customizer
    
    The following tags were removed from style.css because they are deprecated:
    responsive-layout, red, gray, black

--- functions.php
+++ functions.php
@@ -15,3 +15,51 @@ function spyglass_theme_setup() {
     load_child_theme_textdomain( 'spyglass', get_stylesheet_directory() . '/languages' );
 }
 add_action( 'after_setup_theme', 'spyglass_theme_setup' );
+
+/**
+ * Notice in Customize to announce the theme is not maintained anymore
+ */
+function spyglass_customize_register( $wp_customize ) {
+
+	require_once get_stylesheet_directory() . '/class-ti-notify.php';
+
+	$wp_customize->register_section_type( 'Ti_Notify' );
+
+	$wp_customize->add_section(
+		new Ti_Notify(
+			$wp_customize,
+			'ti-notify',
+			array( /* translators: link to our latest theme */
+				'text'     => sprintf( __( 'This theme is not maintained anymore, check-out our latest free one-page theme: %1$s.','spyglass' ), sprintf( '<a href="' . admin_url( 'theme-install.php?theme=hestia' ) . '">%s</a>', 'Hestia' ) ),
+				'priority' => 0,
+			)
+		)
+	);
+
+	$wp_customize->add_setting( 'spyglass-notify', array(
+		'sanitize_callback' => 'esc_html',
+	) );
+
+	$wp_customize->add_control( 'spyglass-notify', array(
+		'label'    => __( 'Notification', 'spyglass' ),
+		'section'  => 'ti-notify',
+		'priority' => 1,
+	) );
+}
+add_action( 'customize_register', 'spyglass_customize_register' );
+
+/**
+ * Notice in admin dashboard to announce the theme is not maintained anymore
+ */
+function spyglass_admin_notice() {
+
+	global $pagenow;
+
+	if ( is_admin() && ( 'themes.php' == $pagenow ) && isset( $_GET['activated'] ) ) {
+		echo '<div class="updated notice is-dismissible"><p>';
+		/* translators: link to our latest theme */
+		printf( __( 'This theme is not maintained anymore, check-out our latest free one-page theme: %1$s.','spyglass' ), sprintf( '<a href="' . admin_url( 'theme-install.php?theme=hestia' ) . '">%s</a>', 'Hestia' ) );
+		echo '</p></div>';
+	}
+}
+add_action( 'admin_notices', 'spyglass_admin_notice', 99 );
